ccdb6433c89f1937bad9c9ac691d956d6dd7d561
smw22 <serwes01@easv365.dk>
2024-09-09 12:49:07 +0200

    push

--- functions.php
+++ functions.php
@@ -2,4 +2,4 @@
 function theme_load_styles_and_scripts() {
     wp_enqueue_style("theme_styles", get_template_directory_uri(). "/style.css");
 }
-add_action("wp_enqueue_scripts","faq_theme_load_styles_and_scripts");
+add_action("wp_enqueue_scripts","theme_load_styles_and_scripts");
