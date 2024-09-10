8cdea97c2ccbb6dd2ac1b62808603060d15d6f4f
smw22 <serwes01@easv365.dk>
2024-09-10 11:01:04 +0200

    explain it

--- header.php
+++ header.php
@@ -13,10 +13,10 @@
         <?php 
             wp_nav_menu(
                 array(
-                'menu' => 'primary',
-                'container' =>'',
-                'theme_location' => 'primary',
-                'items_wrap' => '<ul id="" class="">%3$s</ul>'
+                    'menu' => 'primary',                 // Refers to the name or slug of the menu you want to display. In this case, it's 'primary'.
+                    'container' => '',                   // Removes the default <div> container that WordPress usually wraps around the menu. By setting this to an empty string, no extra container is added.
+                    'theme_location' => 'primary',       // Refers to a specific location in the theme where the menu should appear. 'primary' refers to the registered location, often set in 'functions.php'.
+                    'items_wrap' => '<ul id="" class="">%3$s</ul>' // Defines how the menu items should be wrapped. In this case, an <ul> without an ID or class is used to wrap the menu items.
                 )
             );
         ?>
