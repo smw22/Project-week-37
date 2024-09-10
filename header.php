55f0db3c5ed1d415a602eaa8513dd8eeaaf18e3f
jako3501 <jako3501@easv365.dk>
2024-09-10 12:39:44 +0200

    Update header.php

--- header.php
+++ header.php
@@ -10,16 +10,21 @@
 
 <body>
     <header>
-        <?php 
-            wp_nav_menu(
-                array(
-                    'menu' => 'primary',                 // Refers to the name or slug of the menu you want to display. In this case, it's 'primary'.
-                    'container' => '',                   // Removes the default <div> container that WordPress usually wraps around the menu. By setting this to an empty string, no extra container is added.
-                    'theme_location' => 'primary',       // Refers to a specific location in the theme where the menu should appear. 'primary' refers to the registered location, often set in 'functions.php'.
-                    'items_wrap' => '<ul id="" class="">%3$s</ul>' // Defines how the menu items should be wrapped. In this case, an <ul> without an ID or class is used to wrap the menu items.
-                )
-            );
-        ?>
+        <nav>
+            <ul>
+                <li><a href=""></a></li>
+                <?php
+                wp_nav_menu(
+                    array(
+                        'menu' => 'primary',                 // Refers to the name or slug of the menu you want to display. In this case, it's 'primary'.
+                        'container' => '',                   // Removes the default <div> container that WordPress usually wraps around the menu. By setting this to an empty string, no extra container is added.
+                        'theme_location' => 'primary',       // Refers to a specific location in the theme where the menu should appear. 'primary' refers to the registered location, often set in 'functions.php'.
+                        'items_wrap' => '<ul id="" class="">%3$s</ul>' // Defines how the menu items should be wrapped. In this case, an <ul> without an ID or class is used to wrap the menu items.
+                    )
+                );
+                ?>
+            </ul>
+        </nav>
         <h1>Project Hellooo</h1>
 
     </header>
