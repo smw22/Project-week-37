27344ac0b2ebf1e1298c094c26dd760c5e9825b8
smw22 <serwes01@easv365.dk>
2024-09-10 12:36:43 +0200

    form

--- page-contact.php
+++ page-contact.php
@@ -3,6 +3,7 @@
         <?php while(have_posts()): the_post() ?>
 
             <h1>Helloooo contact</h1>
+            <?php echo do_shortcode('[contact-form-7 id="f850d69" title="Contact form"]') ?>
 
 
         <?php endwhile ?>
