b5d55ab838eb73560d1ef5e113fb34a627a33bfe
jako3501 <jako3501@easv365.dk>
2024-09-11 12:01:37 +0200

    Container style

--- front-page.php
+++ front-page.php
@@ -21,7 +21,7 @@
                     <img src="https://picsum.photos/1280/720" alt="">
                 </div>
                 <div class="item-text">
-                <H2>HEADLINE</H2>
+                    <H2>HEADLINE</H2>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt officiis sequi fugiat numquam fugit
                         nostrum corrupti, reprehenderit nam sit repellendus ab ipsam debitis possimus sint unde exercitationem
                         sunt modi amet.</p>
