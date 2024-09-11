6128f099155939ecb9f86a25710f4ffcd20edb03
jako3501 <jako3501@easv365.dk>
2024-09-11 12:03:52 +0200

    Css changes

--- front-page.php
+++ front-page.php
@@ -16,6 +16,19 @@
 
 
         <div class="container">
+            
+            <div class="item">
+                <div class="item-img">
+                    <img src="https://picsum.photos/1280/720" alt="">
+                </div>
+                <div class="item-text">
+                    <H2>HEADLINE</H2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt officiis sequi fugiat numquam fugit
+                        nostrum corrupti, reprehenderit nam sit repellendus ab ipsam debitis possimus sint unde exercitationem
+                        sunt modi amet.</p>
+                </div>
+            </div>
+
             <div class="item">
                 <div class="item-img">
                     <img src="https://picsum.photos/1280/720" alt="">
