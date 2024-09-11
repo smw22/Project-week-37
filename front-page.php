41e749b0015eb8cd76cfa186bc80f2ef2badb45d
jako3501 <jako3501@easv365.dk>
2024-09-11 11:06:35 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -3,13 +3,13 @@
     <?php while (have_posts()):
         the_post() ?>
 
-        <?php $title = get_the_field('hero-title') ?>
+        <?php $title = get_the_field('hero-title'); ?>
 
 
 
         <div class="hero-image">
             <div class="hero-text">
-                <h1><?php echo $title ?></h1>
+                <h1><?php echo $title; ?></h1>
             </div>
             <img src="https://picsum.photos/1280/720" alt="">
         </div>
