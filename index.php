7357a760882255f66178ccc653dc2fea81be6de8
smw22 <serwes01@easv365.dk>
2024-09-11 09:26:52 +0200

    adaf

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
     <?php if(have_posts()): ?>
         <?php while(have_posts()): the_post() ?>
 
-            <h1>Helloooo Index</h1>
-            <div class="container"></div>
-            <label for="input">Hellooo</label>
-            <input type="text" id="input" placeholder="Hellooo" />
 
         <?php endwhile ?>
     <?php endif ?>
