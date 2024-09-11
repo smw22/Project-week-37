65de7b930f4c22eb329f2daaee62e5676b1a363d
smw22 <serwes01@easv365.dk>
2024-09-11 10:55:41 +0200

    Update page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -4,9 +4,7 @@
 
             <div class="contact-container">
                 <h1>Contact Us</h1>
-                <form action="" class="contact-form">
                 <?php echo do_shortcode('[contact-form-7 id="f850d69" title="Contact form"]') ?>
-                </form>
             </div>
             
         <?php endwhile ?>
