f2e773f961eec80931ca1e841b410a91233d6bcf
smw22 <serwes01@easv365.dk>
2024-09-11 11:24:28 +0200

    style-form

--- page-contact.php
+++ page-contact.php
@@ -4,6 +4,9 @@
 
             <div class="contact-container">
                 <h1>Contact Us</h1>
+                <p>Need to get in touch with us? Fill out the form
+                    with tour inquirity or find the <a href=""> Department of Psychiatric Help </a> that you will like to contact.
+                </p>
                 <?php echo do_shortcode('[contact-form-7 id="f850d69" title="Contact form"]') ?>
             </div>
             
