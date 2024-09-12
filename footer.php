1006209557f679e1ba7b09fdd59432388723de86
smw22 <serwes01@easv365.dk>
2024-09-12 09:36:50 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,42 @@
+<footer>
+    <div class="footer-list-box">
+        <div class="footer-list">
+            <h3>Programs</h3>
+            <p>Best practice Communication</p>
+            <p>Families and caregivers</p>
+            <p>Strategy and Policy</p>
+            <p>Community</p>
+        </div>
+
+        <div class="footer-list">
+            <h3>Prevencion in practice</h3>
+            <p>Resources</p>
+            <p>Understanding mental health</p>
+            <p>Promotion approaches</p>
+            <p>Prevention in practice</p>
+        </div>
+
+        <div class="footer-list">
+            <h3>About</h3>
+            <p>Our Institute</p>
+            <p>Upcoming events</p>
+            <p>Contact</p>
+        </div>
+
+        <div class="footer-list">
+            <h3>Subscribe to Our news letter</h3>
+            <input type="text" placeholder="Your email adress" />
+            <button>Sign Up</button>
+        </div>
+    </div>
+
+    <div class="footer-partners">
+
+    </div>
+</footer>
+
+
+
     <?php wp_footer() ?>
 </body>
 </html>
