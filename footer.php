7a33f24360449d4a562001d9c17030270d0beadb
smw22 <serwes01@easv365.dk>
2024-09-12 10:07:10 +0200

    f

--- footer.php
+++ footer.php
@@ -26,7 +26,7 @@
         <div class="footer-list">
             <h3 class="footer-title">Subscribe to Our news letter</h3>
             <div class="news-letter">
-                <input type="text" class="footer-input" placeholder="Your email adress" />
+                <input type="text" id="footer-input" placeholder="Your email adress" />
                 <button class="footer-btn">Sign Up</button>
             </div>
         </div>
