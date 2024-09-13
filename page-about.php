039c79b3c9aff103e3e7261742fb8d74e7a70d45
jako3501 <jako3501@easv365.dk>
2024-09-13 09:55:23 +0200

    about page dynamic

--- page-about.php
+++ page-about.php
@@ -3,17 +3,27 @@
         <?php while(have_posts()): the_post() ?>
 
             <div class="about-container">
+
+            <?php 
+            $about_title = get_field("about_headline");
+            $about_section = get_field("about_section");
+            $about_section2 = get_field("about-section2");
+            $about_section3 = get_field("about-section3");
+            $about_section4 = get_field("about-section4");
+
+            ?>
+
                 <div class="about-introduction">
                     <h1>About Us</h1>
                     <p>Welcome to Good Health & Well Being, where our mission is to champion mental health and well-being as a core pillar of a thriving future.</p>       
                 </div>
                 <div class="about-section">
                     <div class="about-description">
-                        <h2 class="about-description-title">Welcome to Good Health & Well Being—where your mental health journey begins.</h2>
-                        <p class="about-description-text">At Good Health & Well Being, we are dedicated to addressing the mental health challenges faced by Generation Z and Generation Alpha. These young generations hold the promise of shaping a brighter world, and we believe that ensuring their mental well-being is crucial for this vision to become a reality.</p>
-                        <p class="about-description-text">Our platform is more than just a resource; it’s a movement towards a more informed and empathetic society. We provide valuable insights, practical tools, and a supportive community designed to empower young people in their journey towards mental wellness.</p>
-                        <p class="about-description-text">Rooted in the United Nations Sustainable Development Goal 3, our focus is on creating a space where mental health is prioritized and destigmatized. Through engaging content, expert advice, and interactive features, we aim to spark conversations, offer support, and drive positive change.</p>
-                        <p class="about-description-text">Together, we can build a future where mental health is valued, understood, and actively supported. Join us in making a difference for the generations to come.</p>
+                        <h2 class="about-description-title"><?php echo $about_title ?></h2>
+                        <p class="about-description-text"><?php echo $about_section ?></p>
+                        <p class="about-description-text"><?php echo $about_section2 ?></p>
+                        <p class="about-description-text"><?php echo $about_section3 ?></p>
+                        <p class="about-description-text"><?php echo $about_section4 ?></p>
                     </div>
                     <div class="about-image-box">
                         <img src="<?php echo get_template_directory_uri(); ?>/img/about-image.png" alt="" class="about-image">
