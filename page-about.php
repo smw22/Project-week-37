648b3d25e3a3ee055082340386e1ced7b973f110
jako3501 <jako3501@easv365.dk>
2024-09-13 10:09:29 +0200

    Update page-about.php

--- page-about.php
+++ page-about.php
@@ -1,10 +1,11 @@
 <?php get_header() ?>
-    <?php if(have_posts()): ?>
-        <?php while(have_posts()): the_post() ?>
+<?php if (have_posts()): ?>
+    <?php while (have_posts()):
+        the_post() ?>
 
-            <div class="about-container">
+        <div class="about-container">
 
-            <?php 
+            <?php
 
             $about_headline = get_field("about_headline");
             $about_introtext = get_field("about_introtext");
@@ -16,26 +17,26 @@
 
             ?>
 
-                <div class="about-introduction">
-                    <h1>About Us</h1>
-                    <p>Welcome to Good Health & Well Being, where our mission is to champion mental health and well-being as a core pillar of a thriving future.</p>       
+            <div class="about-introduction">
+                <h1><?php echo $about_headline ?></h1>
+                <p><?php echo $about_introtext ?></p>
+            </div>
+            <div class="about-section">
+                <div class="about-description">
+                    <h2 class="about-description-title"><?php echo $about_subheadline ?></h2>
+                    <p class="about-description-text"><?php echo $about_section ?></p>
+                    <p class="about-description-text"><?php echo $about_section2 ?></p>
+                    <p class="about-description-text"><?php echo $about_section3 ?></p>
+                    x<p class="about-description-text"><?php echo $about_section4 ?></p>
                 </div>
-                <div class="about-section">
-                    <div class="about-description">
-                        <h2 class="about-description-title"><?php echo $about_subheadline ?></h2>
-                        <p class="about-description-text"><?php echo $about_section ?></p>
-                        <p class="about-description-text"><?php echo $about_section2 ?></p>
-                        <p class="about-description-text"><?php echo $about_section3 ?></p>
-                        <p class="about-description-text"><?php echo $about_section4 ?></p>
-                    </div>
-                    <div class="about-image-box">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/about-image.png" alt="" class="about-image">
-                    </div>
+                <div class="about-image-box">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/about-image.png" alt="" class="about-image">
                 </div>
-
             </div>
 
+        </div>
 
-        <?php endwhile ?>
-    <?php endif ?>
+
+    <?php endwhile ?>
+<?php endif ?>
 <?php get_footer() ?>
